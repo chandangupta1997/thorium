4ffe4bb7f4fcd77b2e666bac0c2fe26853a7c88a
chandangupta1997 <chandangupta8000@gmail.com>
2022-03-03 09:07:56 +0530

    workingdone

--- src/controllers/publisherController.js
+++ src/controllers/publisherController.js
@@ -1,27 +1,9 @@
-const { count } = require("console")
-const authorModel = require("../models/authorModel")
-const bookModel= require("../models/bookModel")
-const publisherModel=require("../models/publisherModel")
-
-
+const publisherModel= require("../models/publisherModel")
 
 const createPublisher= async function (req, res) {
-    let author = req.body
-    let publisherCreated = await AuthorModel.create(author)
+    let publisher = req.body
+    let publisherCreated = await publisherModel.create(publisher)
     res.send({data: publisherCreated})
 }
 
-
-const getPublisherData= async function (req, res) {
-    
-    let publisherData = await publisherModel.model(find)
-    res.send({data: publisherData})
-}
-
-
-
-
 module.exports.createPublisher= createPublisher
-module.exports.getPublisherData=getPublisherData
-
-
