d9943e9728b50218309827ed6bc382f1bf74cfc8
chandangupta1997 <chandangupta8000@gmail.com>
2022-03-05 00:38:00 +0530

    goodone

--- src/index.js
+++ src/index.js
@@ -8,20 +8,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use( function (req,res,next){
-    let current_datetime=new Date();
+    //let current_datetime=new Date();
+   /// let formatted_date=current_datetime.getFullYear() +
+    //"_"+
+   // (current_datetime.getMonth()+1)+
+   // current_datetime.getDate()+
+    
+   // current_datetime.getHours()+
 
+    //current_datetime.getMinutes()+
+   // current_datetime.getSeconds();
    let socket =req.socket.remoteAddress // inbuild 
 
     let url=req.url;
 
+    //let log =`[${formatted_date}]${method}:${url}}`
+
+
     
-    console.log(socket,url)
+    console.log(socket,url,)
 
     
     res.send({msg:"you are in global middleware "})
    next()
 })
-
+8
 
 //app.use("/semiware",)
 
